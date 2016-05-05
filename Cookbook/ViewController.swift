d2fb53c0a800b59dc0a4ba434c3906ea8c4d6623
jacwkang <jacwkang@gmail.com>
2016-05-04 22:48:56 -0700

    Initial Commit

---
+++ Cookbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cookbook
+//
+//  Created by Jack Wang on 5/4/16.
+//  Copyright (c) 2016 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
